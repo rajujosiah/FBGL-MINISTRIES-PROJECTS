a69d3d66916b7081b3a62b9a012b3d03de483b11
benayaram-sandspace <benayaram.sandspace@gmail.com>
2026-07-12 17:53:41 +0530

    Update MongoDB URI to point to correct database

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 // Database Connection (MongoDB)
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://fbglministries2016_db_user:LuyNgH2gPY7fuOi9@cluster0.zw4qyzh.mongodb.net/?appName=Cluster0';
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://fbglministries2016_db_user:LuyNgH2gPY7fuOi9@cluster0.zw4qyzh.mongodb.net/fbgl?retryWrites=true&w=majority&appName=Cluster0';
 const localMongoURI = 'mongodb://127.0.0.1:27017/fbgl';
 
 mongoose.connect(mongoURI)
